090b839b9f884b9bdd7d9b26334dda21e247bd21
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 08:29:55 -0500

    Add files via upload

---
+++ Beginning-Javascript/switch-case/app.js
@@ -0,0 +1,36 @@
+const day = 2;
+switch (day) {
+	case 1:
+		console.log("Monday");
+		break;
+	case 2:
+		console.log("Tuesday");
+		break;
+	case 3:
+		console.log("Wednesday");
+		break;
+	case 4:
+		console.log("Thursday");
+		break;
+	case 5:
+		console.log("Friday");
+		break;
+	case 6:
+	case 7:
+		console.log("WEEKEND!")
+		break;
+	default:
+		console.log("I Don't Know That!")
+}
+
+// if (day === 1) {
+// 	console.log ("Monday")
+// } else if (day === 2) {
+// 	console.log ("Tuesday")
+// } else if (day === 3) {
+// 	console.log ("Wednesday")
+// } else if (day === 4) {
+// 	console.log ("Thursday")
+// } else if (day === 5) {
+// 	console.log ("Friday")
+// }
