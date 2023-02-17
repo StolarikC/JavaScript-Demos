a8ebdcffd5ebe82e941f258a4336a93198499d1e
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 08:56:03 -0500

    Add files via upload

---
+++ Callbacks and Array Methods/Map Method/app.js
@@ -0,0 +1,10 @@
+// Example Below
+const texts = ['rofl', 'lol', 'omg', 'ttyl']
+const caps = texts.map(function (t) {
+    return t.toUpperCase();
+})
+texts; 
+// ["rofl", "lol", "omg", "ttyl"]
+caps; 
+// ["ROFL", "LOL", "OMG", "TTYL"]
+
