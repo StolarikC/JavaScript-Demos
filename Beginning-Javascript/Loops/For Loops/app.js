90987b204ebaf813c584a939fefaeb900912c0c8
StolarikC <93167335+StolarikC@users.noreply.github.com>
2023-02-17 08:37:31 -0500

    Add files via upload

---
+++ Beginning-Javascript/Loops/For Loops/app.js
@@ -0,0 +1,58 @@
+//console.log(1);
+//console.log(2);
+//console.log(3);
+//console.log(4);
+//console.log(5);
+//console.log(6);
+//console.log(7);
+//console.log(8);
+//console.log(9);
+//console.log(10);
+
+//USE A 'FOR LOOP' FOR FEWER LINES OF CODE for the same result.
+
+//for (let i = 1; i <= 10; i++) {
+//	console.log("I'M BEFORE THE 'i' LOOP!");
+//	console.log(i);
+//	console.log("I'M AFTER THE 'i' LOOP!");
+//}
+
+//console.log("Eiffel 65 -- 'I'm Blue'")
+////let chorus = "Da ba dee da ba daa"; <---could go here.
+
+//for (let i = 1; i <= 6; i++) {
+//	let chorus = "Da ba dee da ba daa"; //<-- or here.
+//	console.log(chorus);
+//}
+
+////EVEN NUMBERS
+//for (let i = 0; i <= 20; i+=2){
+//	console.log(i);
+//}
+
+////ODD NUMBERS
+//for (let i = 1; i <= 20; i+=2){
+//	console.log(i);
+//}
+
+////100 to 0 down by TENS
+// for (i=100; i>=0; i-=10){
+// 	console.log(i);
+// }
+
+////EXPONENTS OF 2 <=1000
+// for (i=2; i<=1000; i*=2){
+// 	console.log(i);
+// }
+
+// LOOPING OVER ARRAYS
+// const animals = ['lions', 'tigers', 'bears'];
+
+// for (let i = 0; i < animals.length; i++) {
+// 	console.log(i, animals[i]);
+// }
+
+// //REVERSE ORDER
+// for (let i=animals.length-1; i>=0; i--){
+// 	console.log(i, animals[i]);
+// }
